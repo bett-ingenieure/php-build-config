e9eb410a37249db25e7afd1f5783b7fe915efe05
fbett <info@bett-ingenieure.de>
2021-12-06 10:54:19 +0100

    error management + automatic fetch

--- php7_4-update.sh
+++ php7_4-update.sh
@@ -2,9 +2,8 @@
 
 # go to "php-7": the working directory
 cd ..
-if [ ! -e "php-7.4" ]
-then
-  mkdir "php-7.4"
+if [ ! -e "php-7.4" ]; then
+    mkdir "php-7.4"
 fi
 cd php-7.4
 
@@ -12,155 +11,182 @@ cd php-7.4
 read -p "Update build package dependencies? (y/n)?" RESPONSE
 if [ "$RESPONSE" = "y" ]; then
 
-  apt-get update
-  # PHP7 needed, here are only the additional packages listed
-  apt-get install libsqlite3-dev libonig-dev libwebp-dev libxpm-dev
+    apt-get update
+    # PHP7 needed, here are only the additional packages listed
+    apt-get install libsqlite3-dev libonig-dev libwebp-dev libxpm-dev
 fi
 
 ### WORKFLOW: ASK VERSION ###
-VERSION=
-while [ -z $VERSION ]
-do
-    echo -n 'Version? f.e. 7.4.1 '
-    read VERSION
-done
+#VERSION=
+#while [ -z $VERSION ]; do
+#    echo -n 'Version? f.e. 7.4.1 '
+#    read VERSION
+#done
+
+DIRECTORY_TARGET=
 
 ### WORKFLOW: FETCH ###
-read -p "Fetch now? (y/n)?" RESPONSE
-if [ "$RESPONSE" = "y" ]; then
 
-  if [ -e "mirror" ]
-  then
-    rm mirror
-  fi
-
-  wget http://de2.php.net/get/php-${VERSION}.tar.gz/from/this/mirror
-  mv mirror php-${VERSION}.tar.gz
-  tar xfvz php-${VERSION}.tar.gz
-  chown -R root:root php-${VERSION}
-
-  rm php-${VERSION}.tar.gz
+if [ -e "download" ]; then
+    rm download
 fi
 
+wget -O download https://config.bett-ingenieure.de/getLatestVersion?target=php-7.4.tar.gz
+
+if [ $? -ne 0 ]; then
+    echo "failed to fetch" >&2
+    exit 1
+fi
+
+DIRECTORY_TARGET=$(tar -tzf download | head -1 | cut -f1 -d"/")
+
+if [ -e "${DIRECTORY_TARGET}" ]; then
+    rm -R ${DIRECTORY_TARGET}
+fi
+
+tar xfvz download
+
+if [ $? -ne 0 ]; then
+    echo "failed to untar" >&2
+    exit 1
+fi
+
+chown -R root:root ${DIRECTORY_TARGET}
+
+rm download
+
+cd ./${DIRECTORY_TARGET}
+
 ### WORKFLOW: CONFIGURE ###
-read -p "Configure now? (y/n)?" RESPONSE
-if [ "$RESPONSE" = "y" ]; then
-  cd ./php-${VERSION}
 
 ./configure \
-  --prefix=/opt/php-7.4/php-${VERSION}/ \
-  --enable-mbstring \
-  --enable-soap \
-  --enable-calendar \
-  --with-curl \
-  --enable-gd \
-  --with-freetype \
-  --disable-rpath \
-  --enable-inline-optimization \
-  --with-bz2 \
-  --with-zlib \
-  --with-zip \
-  --with-pear \
-  --enable-sockets \
-  --enable-sysvsem \
-  --enable-sysvshm \
-  --enable-pcntl \
-  --enable-mbregex \
-  --with-mhash \
-  --with-pdo-mysql \
-  --with-mysqli \
-  --with-jpeg=/usr/include/ \
-  --with-xpm=/usr/include/ \
-  --with-webp=/usr/include/ \
-  --with-openssl \
-  --with-fpm-user=www-data \
-  --with-fpm-group=www-data \
-  --with-libdir=/lib/x86_64-linux-gnu \
-  --enable-ftp \
-  --with-imap \
-  --with-imap-ssl \
-  --with-kerberos \
-  --with-gettext \
-  --with-libxml \
-  --enable-fpm \
-  --enable-intl \
-  --enable-bcmath \
-  --enable-exif \
-  --enable-shmop \
-  --enable-sysvmsg \
-  --with-xmlrpc \
-  --with-pear
+    --prefix=/opt/php-7.4/${DIRECTORY_TARGET}/ \
+    --enable-mbstring \
+    --enable-soap \
+    --enable-calendar \
+    --with-curl \
+    --enable-gd \
+    --with-freetype \
+    --disable-rpath \
+    --enable-inline-optimization \
+    --with-bz2 \
+    --with-zlib \
+    --with-zip \
+    --with-pear \
+    --enable-sockets \
+    --enable-sysvsem \
+    --enable-sysvshm \
+    --enable-pcntl \
+    --enable-mbregex \
+    --with-mhash \
+    --with-pdo-mysql \
+    --with-mysqli \
+    --with-jpeg=/usr/include/ \
+    --with-xpm=/usr/include/ \
+    --with-webp=/usr/include/ \
+    --with-openssl \
+    --with-fpm-user=www-data \
+    --with-fpm-group=www-data \
+    --with-libdir=/lib/x86_64-linux-gnu \
+    --enable-ftp \
+    --with-imap \
+    --with-imap-ssl \
+    --with-kerberos \
+    --with-gettext \
+    --with-libxml \
+    --enable-fpm \
+    --enable-intl \
+    --enable-bcmath \
+    --enable-exif \
+    --enable-shmop \
+    --enable-sysvmsg \
+    --with-xmlrpc \
+    --with-pear
 
-  cd ..
+if [ $? -ne 0 ]; then
+    echo "failed to configure" >&2
+    exit 1
 fi
 
 ### WORKFLOW: MAKE ###
-read -p "make now? (y/n)?" RESPONSE
-if [ "$RESPONSE" = "y" ]; then
-  cd ./php-${VERSION}
 
-  make -j 4
+make -j 4
 
-  cd ..
+if [ $? -ne 0 ]; then
+    echo "failed to make" >&2
+    exit 1
 fi
 
 ### WORKFLOW: INSTALL ###
-read -p "make install now? (y/n)?" RESPONSE
-if [ "$RESPONSE" = "y" ]; then
-  cd ./php-${VERSION}
 
-  make install
+make install
 
-  cd ..
+if [ $? -ne 0 ]; then
+    echo "failed to install" >&2
+    exit 1
 fi
 
 ### WORKFLOW: EXTENSIONS ###
-read -p "should install extensions? (y/n)?" RESPONSE
-if [ "$RESPONSE" = "y" ]; then
-  cd ./php-${VERSION}/bin
 
-  ./pecl -C ./pear.conf update-channels
+cd ./bin
 
-  echo Will install imagick...
-  sleep 2s
-  ./pecl install imagick
+./pecl -C ./pear.conf update-channels
 
-  echo Will install apcu...
-  sleep 2s
-  ./pecl install apcu
-  ./pecl install apcu_bc-beta
-
-  echo Will install redis...
-  sleep 2s
-  ./pecl install redis
-
-  cd ..
-  cd ..
-
+if [ $? -ne 0 ]; then
+    echo "Failed to update pecl" >&2
+    exit 1
 fi
+
+echo Will install imagick...
+yes '' | ./pecl install imagick
+
+if [ $? -ne 0 ]; then
+    echo "Failed to install imagick via pecl" >&2
+    exit 1
+fi
+
+echo Will install apcu...
+yes '' | ./pecl install apcu
+yes '' | ./pecl install apcu_bc-beta
+
+if [ $? -ne 0 ]; then
+    echo "Failed to install imagick apcu via pecl" >&2
+    exit 1
+fi
+
+echo Will install redis...
+yes '' | ./pecl install redis
+
+if [ $? -ne 0 ]; then
+    echo "Failed to install redis via pecl" >&2
+    exit 1
+fi
+
+cd ..
+
+### WORKFLOW: ACTIVATE ###
 
 read -p "No errors? Should activate? (y/n)?" RESPONSE
 if [ "$RESPONSE" = "y" ]; then
 
-  INIT_SCRIPT="/etc/init.d/php7.4-fpm"
+    INIT_SCRIPT="/etc/init.d/php7.4-fpm"
 
-  # update init script
-  if [ -e "$INIT_SCRIPT" ]
-  then
-    "$INIT_SCRIPT" stop
-    rm "$INIT_SCRIPT"
-  fi
+    # update init script
+    if [ -e "$INIT_SCRIPT" ]; then
+        "$INIT_SCRIPT" stop
+        rm "$INIT_SCRIPT"
+    fi
 
-  ln -s /opt/php-build-config/php7_4-fpm "$INIT_SCRIPT"
+    ln -s /opt/php-build-config/php7_4-fpm "$INIT_SCRIPT"
 
-  cd php-${VERSION}/lib/
-  ln -s ../../../php-build-config/php7.ini php.ini
-  cd ..
-  cd ..
+    cd lib/
+    ln -s ../../../php-build-config/php7.ini php.ini
+    cd ..
+    cd ..
 
-  rm current-live
-  ln -s php-${VERSION} current-live
+    rm current-live
+    ln -s ${DIRECTORY_TARGET} current-live
 
-  "$INIT_SCRIPT" start
-  
+    "$INIT_SCRIPT" start
+
 fi
