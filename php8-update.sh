d5a2c738d690aaa836ef4065a06ad08b2404b905
fbett <info@bett-ingenieure.de>
2021-12-06 11:30:08 +0100

    error management + automatic fetch	e9eb410	fbett <info@bett-ingenieure.de>	06.12.2021, 10:54

--- php8-update.sh
+++ php8-update.sh
@@ -19,135 +19,164 @@ if [ "$RESPONSE" = "y" ]; then
 fi
 
 ### WORKFLOW: ASK VERSION ###
-VERSION=
-while [ -z $VERSION ]; do
-    echo -n 'Version? f.e. 8.0.0 '
-    read VERSION
-done
+#VERSION=
+#while [ -z $VERSION ]; do
+#    echo -n 'Version? f.e. 8.0.0 '
+#    read VERSION
+#done
+
+DIRECTORY_TARGET=
 
 ### WORKFLOW: FETCH ###
-read -p "Fetch now? (y/n)?" RESPONSE
-if [ "$RESPONSE" = "y" ]; then
 
-    if [ -e "mirror" ]; then
-        rm mirror
-    fi
-
-    wget http://de2.php.net/get/php-${VERSION}.tar.gz/from/this/mirror
-    mv mirror php-${VERSION}.tar.gz
-    tar xfvz php-${VERSION}.tar.gz
-    chown -R root:root php-${VERSION}
-
-    rm php-${VERSION}.tar.gz
+if [ -e "download" ]; then
+    rm download
 fi
 
+wget -O download https://config.bett-ingenieure.de/getLatestVersion?target=php-8.0.tar.gz
+
+if [ $? -ne 0 ]; then
+    echo "failed to fetch" >&2
+    exit 1
+fi
+
+DIRECTORY_TARGET=$(tar -tzf download | head -1 | cut -f1 -d"/")
+
+if [ -e "${DIRECTORY_TARGET}" ]; then
+    rm -R ${DIRECTORY_TARGET}
+fi
+
+tar xfvz download
+
+if [ $? -ne 0 ]; then
+    echo "failed to untar" >&2
+    exit 1
+fi
+
+chown -R root:root ${DIRECTORY_TARGET}
+
+rm download
+
+cd ./${DIRECTORY_TARGET}
+
 ### WORKFLOW: CONFIGURE ###
-read -p "Configure now? (y/n)?" RESPONSE
-if [ "$RESPONSE" = "y" ]; then
-    cd ./php-${VERSION}
 
-    ./configure \
-        --prefix=/opt/php-8/php-${VERSION}/ \
-        --enable-mbstring \
-        --enable-soap \
-        --enable-calendar \
-        --with-curl \
-        --enable-gd \
-        --with-freetype \
-        --disable-rpath \
-        --with-bz2 \
-        --with-zlib \
-        --with-zip \
-        --with-pear \
-        --enable-sockets \
-        --enable-sysvsem \
-        --enable-sysvshm \
-        --enable-pcntl \
-        --enable-mbregex \
-        --with-mhash \
-        --with-pdo-mysql \
-        --with-mysqli \
-        --with-jpeg=/usr/include/ \
-        --with-xpm=/usr/include/ \
-        --with-webp=/usr/include/ \
-        --with-openssl \
-        --with-fpm-user=www-data \
-        --with-fpm-group=www-data \
-        --with-libdir=/lib/x86_64-linux-gnu \
-        --enable-ftp \
-        --with-imap \
-        --with-imap-ssl \
-        --with-kerberos \
-        --with-gettext \
-        --with-libxml \
-        --enable-fpm \
-        --enable-intl \
-        --enable-bcmath \
-        --enable-exif \
-        --enable-shmop \
-        --enable-sysvmsg
+./configure \
+    --prefix=/opt/php-8/${DIRECTORY_TARGET}/ \
+    --enable-mbstring \
+    --enable-soap \
+    --enable-calendar \
+    --with-curl \
+    --enable-gd \
+    --with-freetype \
+    --disable-rpath \
+    --with-bz2 \
+    --with-zlib \
+    --with-zip \
+    --with-pear \
+    --enable-sockets \
+    --enable-sysvsem \
+    --enable-sysvshm \
+    --enable-pcntl \
+    --enable-mbregex \
+    --with-mhash \
+    --with-pdo-mysql \
+    --with-mysqli \
+    --with-jpeg=/usr/include/ \
+    --with-xpm=/usr/include/ \
+    --with-webp=/usr/include/ \
+    --with-openssl \
+    --with-fpm-user=www-data \
+    --with-fpm-group=www-data \
+    --with-libdir=/lib/x86_64-linux-gnu \
+    --enable-ftp \
+    --with-imap \
+    --with-imap-ssl \
+    --with-kerberos \
+    --with-gettext \
+    --with-libxml \
+    --enable-fpm \
+    --enable-intl \
+    --enable-bcmath \
+    --enable-exif \
+    --enable-shmop \
+    --enable-sysvmsg
 
-    cd ..
+if [ $? -ne 0 ]; then
+    echo "failed to configure" >&2
+    exit 1
 fi
 
 ### WORKFLOW: MAKE ###
-read -p "make now? (y/n)?" RESPONSE
-if [ "$RESPONSE" = "y" ]; then
-    cd ./php-${VERSION}
 
-    make -j 4
+make -j 4
 
-    cd ..
+if [ $? -ne 0 ]; then
+    echo "failed to make" >&2
+    exit 1
 fi
 
 ### WORKFLOW: INSTALL ###
-read -p "make install now? (y/n)?" RESPONSE
-if [ "$RESPONSE" = "y" ]; then
-    cd ./php-${VERSION}
 
-    make install
+make install
 
-    cd ..
+if [ $? -ne 0 ]; then
+    echo "failed to install" >&2
+    exit 1
 fi
 
 ### WORKFLOW: EXTENSIONS ###
-read -p "should install extensions? (y/n)?" RESPONSE
-if [ "$RESPONSE" = "y" ]; then
-    cd ./php-${VERSION}/bin
 
-    ./pecl -C ./pear.conf update-channels
+cd bin
 
-    echo Will install imagick...
-    sleep 2s
+./pecl -C ./pear.conf update-channels
 
-    ./pecl install imagick
-
-    ###
-    ### ./pecl install imagick
-    ###
-    ### PECL IMAGICK is not compatible (27.11.2020) while installing 3.4.4:
-    ### /tmp/pear/temp/imagick/imagick_file.c:313:112: error: expected ‘;’, ‘,’ or ‘)’ before ‘TSRMLS_DC’
-    ###  zend_bool php_imagick_stream_handler(php_imagick_object *intern, php_stream *stream, ImagickOperationType type TSRMLS_DC)
-    ###                                                                                                                ^~~~~~~~~
-    ### make: *** [Makefile:209: imagick_file.lo] Fehler 1
-    ###
-    ### Fetching master from github?
-
-    ### Same for "apcu_bc-beta"
-    ###
-
-    echo Will install apcu...
-    sleep 2s
-    ./pecl install apcu
-
-    echo Will install redis...
-    sleep 2s
-    ./pecl install redis
-
-    cd ..
-    cd ..
-
+if [ $? -ne 0 ]; then
+    echo "Failed to update pecl" >&2
+    exit 1
 fi
+
+echo Will install imagick...
+yes '' | ./pecl install imagick
+
+if [ $? -ne 0 ]; then
+    echo "Failed to install imagick via pecl" >&2
+    exit 1
+fi
+
+###
+### ./pecl install imagick
+###
+### PECL IMAGICK is not compatible (27.11.2020) while installing 3.4.4:
+### /tmp/pear/temp/imagick/imagick_file.c:313:112: error: expected ‘;’, ‘,’ or ‘)’ before ‘TSRMLS_DC’
+###  zend_bool php_imagick_stream_handler(php_imagick_object *intern, php_stream *stream, ImagickOperationType type TSRMLS_DC)
+###                                                                                                                ^~~~~~~~~
+### make: *** [Makefile:209: imagick_file.lo] Fehler 1
+###
+### Fetching master from github?
+
+### Same for "apcu_bc-beta"
+###
+
+echo Will install apcu...
+yes '' | ./pecl install apcu
+
+if [ $? -ne 0 ]; then
+    echo "Failed to install imagick apcu via pecl" >&2
+    exit 1
+fi
+
+echo Will install redis...
+yes '' | ./pecl install redis
+
+if [ $? -ne 0 ]; then
+    echo "Failed to install redis via pecl" >&2
+    exit 1
+fi
+
+cd ..
+
+### WORKFLOW: ACTIVATE ###
 
 read -p "No errors? Should activate? (y/n)?" RESPONSE
 if [ "$RESPONSE" = "y" ]; then
@@ -163,13 +192,13 @@ if [ "$RESPONSE" = "y" ]; then
     ln -s /opt/php-build-config/php8-fpm "$INIT_SCRIPT"
     chmod +x /opt/php-build-config/php8-fpm
 
-    cd php-${VERSION}/lib/
+    cd lib/
     ln -s ../../../php-build-config/php8.ini php.ini
     cd ..
     cd ..
 
     rm current-live
-    ln -s php-${VERSION} current-live
+    ln -s ${DIRECTORY_TARGET} current-live
 
     "$INIT_SCRIPT" start
 
